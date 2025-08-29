0be52a0d53dfb8e3aa33ca5ffcc60108afd38fbb
Stuart Willows <47339755+BearintheWillows@users.noreply.github.com>
2025-08-29 22:29:39 +0100

    Migrate project from JavaScript to TypeScript, update configurations, enhance dependencies, and add TailwindCSS integration.

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+﻿import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Register from "@/views/Register.vue";
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/register', component: Register }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
